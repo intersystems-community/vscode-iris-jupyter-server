15c3a3141e477b5690584e9c73297e73b18593c7
gjsjohnmurray <johnm@georgejames.com>
2023-05-31 17:58:06 +0100

    Remove helloWorld command

--- src/extension.ts
+++ src/extension.ts
@@ -13,17 +13,6 @@ export function activate(context: vscode.ExtensionContext) {
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "iris-jupyter-server" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('iris-jupyter-server.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Jupyter server proxy for InterSystems IRIS!');
-	});
-
-	context.subscriptions.push(disposable);
-
 	const fastify = Fastify({
 		//logger: true
 	});
